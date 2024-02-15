3d783062f61a77b4ab3da3d424b47ab7e69b3165
bobbytbh <104529152+bobbytbh@users.noreply.github.com>
2024-02-15 20:42:45 +0100

    new function

--- src/functions/getNetEn.py
+++ src/functions/getNetEn.py
@@ -4,59 +4,43 @@ import os
 def getWLAN():
     def get_wifi_profiles():
         try:
-            # Ottieni l'output di 'netsh wlan show profiles'
             result = os.popen('netsh wlan show profiles').read()
-            
-            # Trova tutti i nomi dei profili WiFi nell'output
-            profile_names = re.findall(r'Tutti i profili utente\s*:\s*(.*)', result)
+            profile_names = re.findall(r'All User Profile\s*:\s*(.*)', result)
             return profile_names
-
         except Exception as e:
             print(f"Error during the command execution: {e}")
             return []
 
     def get_wifi_password(profile_name):
         try:
-            # Ottieni l'output di 'netsh wlan show profile name="profile_name" key=clear'
             result = os.popen(f'netsh wlan show profile name="{profile_name}" key=clear').read()
-
-            # Estrai la password dalla riga che contiene 'Contenuto chiave'
-            password_match = re.search(r'Contenuto chiave\s*:\s*(.*)', result)
+            password_match = re.search(r'Key Content\s*:\s*(.*)', result)
             password = password_match.group(1) if password_match else None
             return password
-
         except Exception as e:
             print(f"Error during the command execution: {e}")
             return None
 
-    # Ottieni tutti i profili WiFi
     profile_names = get_wifi_profiles()
-    
-    # Creiamo una lista di stringhe per memorizzare i profili e le relative password
     wifi_profiles = []
 
-    # Per ogni profilo, ottieni la password corrispondente
     for profile_name in profile_names:
         password = get_wifi_password(profile_name)
-
-        # Formatta il nome del profilo e la password e aggiungilo alla lista
         profile_info = f"{profile_name}: {password}" if password else f"{profile_name}: Absent"
         wifi_profiles.append(profile_info)
-    
-    # Unisci tutte le stringhe della lista in un'unica stringa separata da newline
+
     return '\n'.join(wifi_profiles)
 
 def getLAN():
     try:
         output = os.popen("netsh interface show interface").read()
-    #    print("LAN Output:")
     except Exception as e:
-        print("Errore durante l'esecuzione di 'netsh interface show interface':", e)
+        print("Error executing 'netsh interface show interface':", e)
         return []
 
     interfaces = []
     for line in output.split('\n'):
-        if line.startswith("Connessione"):
+        if line.startswith("Connection"):
             interface = line.split()[-1]
             interfaces.append(interface)
 
@@ -65,7 +49,6 @@ def getLAN():
 def getIP():
     try:
         output = os.popen("ipconfig").read()
-    #    print("IPConfig Output:")
     except Exception as e:
         print("Error executing 'ipconfig':", e)
         return {}
@@ -74,17 +57,14 @@ def getIP():
     subnet_mask = "" 
     gateway = ""
 
-    # Utilizziamo le espressioni regolari per cercare i pattern corrispondenti
-    ipv4_pattern = re.compile(r'Indirizzo IPv4[ .]+: ([0-9.]+)')
-    subnet_mask_pattern = re.compile(r'Subnet mask[ .]+: ([0-9.]+)')
-    gateway_pattern = re.compile(r'Gateway predefinito[ .]+: ([0-9.]+)')
+    ipv4_pattern = re.compile(r'IPv4 Address[ .]+: ([0-9.]+)')
+    subnet_mask_pattern = re.compile(r'Subnet Mask[ .]+: ([0-9.]+)')
+    gateway_pattern = re.compile(r'Default Gateway[ .]+: ([0-9.]+)')
 
-    # Cerchiamo le corrispondenze nei dati di output
     ipv4_match = ipv4_pattern.search(output)
     subnet_mask_match = subnet_mask_pattern.search(output)
     gateway_match = gateway_pattern.search(output)
 
-    # Estraiamo i valori corrispondenti se trovati
     if ipv4_match:
         ipv4 = ipv4_match.group(1)
     if subnet_mask_match:
@@ -92,9 +72,9 @@ def getIP():
     if gateway_match:
         gateway = gateway_match.group(1)
 
-    freturn = f"IPv4: {ipv4}\nSubnet mask: {subnet_mask}\nDefault gateway: {gateway}"
+    result = f"IPv4: {ipv4}\nSubnet Mask: {subnet_mask}\nDefault Gateway: {gateway}"
 
-    return freturn
+    return result
 
 def getDNS():
     try:
@@ -105,30 +85,26 @@ def getDNS():
 
     return output
 
-
 def getMAC():
     try:
-        output = os.popen("getmac /fo table /nh").read()  # Utilizziamo il formato tabellare
+        output = os.popen("getmac /fo table /nh").read()
     except Exception as e:
         print("Error executing 'getmac':", e)
         return "Error executing 'getmac': " + str(e)
 
     result = ""
-
-    # Utilizziamo un'espressione regolare per estrarre i nomi dei trasporti
     transport_pattern = re.compile(r"^(.*?)\s{2,}(.*?)$", re.IGNORECASE | re.MULTILINE)
-
     transport_matches = transport_pattern.findall(output)
 
     for transport_match in transport_matches:
-        mac_address = transport_match[0].strip()  # Estraiamo l'indirizzo MAC dalla prima colonna
-        transport_name = transport_match[1].strip()  # Estraiamo il nome del trasporto dalla seconda colonna
-        result += "\n\n"  # Aggiungiamo una riga vuota tra i record
+        mac_address = transport_match[0].strip()
+        transport_name = transport_match[1].strip()
+        result += "\n\n"
         result += "Physical Address: " + mac_address + "\n"
         result += "Transport Name: " + transport_name + "\n"
-        result += "\n"  # Aggiungiamo una riga vuota tra i record
+        result += "\n"
 
-    return result  # Rimuoviamo eventuali spazi vuoti aggiuntivi alla fine
+    return result.strip()
 
 def getNET():
     return f"\n\n\n\nWLAN:\n{getWLAN()}\n\n\nLAN:\n{getLAN()}\n\n\nIP:\n{getIP()}\n\n\nDNS:\n{getDNS()}\n\n\nMAC:\n{getMAC()}"
--- src/main.py
+++ src/main.py
@@ -1,35 +1,76 @@
 import functions.GetNet as gn
+import functions.getNetEn as gne
+from langdetect import detect
+import os
 
 def main():
     print("Icons by: https://icons8.com/")
-    while True:
-        scelta = input("Inserisci '1' per WLAN, '2' per LAN, '3' per IP, '4' per DNS, '5' per MAC, '6' per WLAN, LAN, IP, DNS, MAC, '7' per Esci: ")
-        if scelta == '1':
-            print("Please wait while we try to retrive informations about your WLAN")
-            a = gn.getWLAN()
-            print(a)            
-        elif scelta == '2':
-            print("Please wait while we try to retrive informations about your LAN")
-            a = gn.getLAN()
-            print(a)
-        elif scelta == '3':
-            print("Please wait while we try to retrive informations about your IP")
-            a = gn.getIP()
-            print(a)
-        elif scelta == '4':
-            print("Please wait while we try to retrive informations about your DNS")
-            a = gn.getDNS()
-            print(a)
-        elif scelta == '5':
-            print("Please wait while we try to retrive informations about your MAC")
-            a = gn.getMAC()
-            print(a)
-        elif scelta == '6':
-            print("Please wait while we try to retrive informations")
-            a = gn.getNET()
-            print(a)
-        elif scelta == '7':
-            break
+    # controlla la lingua del sistema operativo
+    lang = detect(os.popen('ipconfig').read())
+    
+    if lang == "it":
+        print("Lingua di sistema rilevata: Italiano")
+        while True:
+            scelta = input("Inserisci '1' per WLAN, '2' per LAN, '3' per IP, '4' per DNS, '5' per MAC, '6' per tutti, '7' per uscire: ")
+            if scelta == '1':
+                print("Attendere mentre proviamo a recuperare informazioni sulla WLAN")
+                a = gn.getWLAN()
+                print(a)            
+            elif scelta == '2':
+                print("Attendere mentre proviamo a recuperare informazioni sulla LAN")
+                a = gn.getLAN()
+                print(a)
+            elif scelta == '3':
+                print("Attendere mentre proviamo a recuperare informazioni sull'IP")
+                a = gn.getIP()
+                print(a)
+            elif scelta == '4':
+                print("Attendere mentre proviamo a recuperare informazioni sul DNS")
+                a = gn.getDNS()
+                print(a)
+            elif scelta == '5':
+                print("Attendere mentre proviamo a recuperare informazioni sul MAC")
+                a = gn.getMAC()
+                print(a)
+            elif scelta == '6':
+                print("Attendere mentre proviamo a recuperare tutte le informazioni")
+                a = gn.getNET()
+                print(a)
+            elif scelta == '7':
+                break
+
+    elif lang == "en":
+        print("Detected system language: English")
+        
+        while True:
+            choice = input("Insert '1' for WLAN, '2' for LAN, '3' for IP, '4' for DNS, '5' for MAC, '6' for all, '7' to exit: ")
+            if choice == '1':
+                print("Please wait while we try to retrive informations about your WLAN")
+                a = gne.getWLAN()
+                print(a)            
+            elif choice == '2':
+                print("Please wait while we try to retrive informations about your LAN")
+                a = gne.getLAN()
+                print(a)
+            elif choice == '3':
+                print("Please wait while we try to retrive informations about your IP")
+                a = gne.getIP()
+                print(a)
+            elif choice == '4':
+                print("Please wait while we try to retrive informations about your DNS")
+                a = gne.getDNS()
+                print(a)
+            elif choice == '5':
+                print("Please wait while we try to retrive informations about your MAC")
+                a = gne.getMAC()
+                print(a)
+            elif choice == '6':
+                print("Please wait while we try to retrive informations")
+                a = gne.getNET()
+                print(a)
+            elif choice == '7':
+                break
+        
 
 if __name__ == '__main__':
     main()
